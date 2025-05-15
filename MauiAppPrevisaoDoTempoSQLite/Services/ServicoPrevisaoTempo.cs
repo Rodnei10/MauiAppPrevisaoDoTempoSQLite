175c1d6299c5415903f3492dbbdd8810ac68cbc0
Rodnei10 <0200832221028@LAB03-02>
2025-05-15 17:39:50 -0300

    atualização

--- MauiAppPrevisaoDoTempoSQLite/Services/ServicoPrevisaoTempo.cs
+++ MauiAppPrevisaoDoTempoSQLite/Services/ServicoPrevisaoTempo.cs
@@ -4,6 +4,8 @@ using Newtonsoft.Json;
 using PrevisaoDoTempoApp.Models;
 using MauiAppPrevisaoDoTempoSQLite.Data;
 using MauiAppPrevisaoDoTempoSQLite.Models;
+using System.Diagnostics;
+using Newtonsoft.Json.Linq;
 
 
 namespace MauiAppPrevisaoDoTempoSQLite.Services
@@ -13,7 +15,7 @@ namespace MauiAppPrevisaoDoTempoSQLite.Services
         private readonly HttpClient _httpClient;
         private readonly string appId = "6135072afe7f6cec1537d5cb08a5a1a2";
         private readonly string baseUrl = "https://api.openweathermap.org/data/2.5/weather";
-        private readonly PrevisaoTempoDatabase _database;//aki
+        private readonly PrevisaoTempoDatabase _database;
 
         public ServicoPrevisaoTempo(PrevisaoTempoDatabase database)
         {
@@ -28,17 +30,34 @@ namespace MauiAppPrevisaoDoTempoSQLite.Services
 
             if (!string.IsNullOrEmpty(response))
             {
+                var rascunho = JObject.Parse(response);
+
+                double temp_min = (double)rascunho["main"]["temp_min"];
+                double temp_max = (double)rascunho["main"]["temp_max"];
+                double temp = (double)rascunho["main"]["temp"];
+
+
+
                 var previsao = JsonConvert.DeserializeObject<PrevisaoTempo>(response);
+
+
                 if (previsao != null)
-                {
-                    await _database.SaveConsultaAsync(new PesquisaPrevisaoTempo
+                {                    
+
+                    int teste = await _database.SaveConsultaAsync(new PesquisaPrevisaoTempo
                     {
                         Cidade = cidade,
-                        DataPesquisa = DateTime.Now
+                        DataPesquisa = DateTime.Now,
+                        Temperatura = temp
                     });
+
+                    Debug.WriteLine("------------------------------------------");
+                    Debug.WriteLine(teste);
+                    Debug.WriteLine("------------------------------------------");
 
                     previsao.Cidade = cidade;
                     previsao.Data = DateTime.Now;
+                    previsao.Temperatura = temp;
                 }
                 return previsao;
             }
